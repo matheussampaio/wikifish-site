d7f1945b5fb6d2d166fad58c6d88d41ba1d95238
Matheus Sampaio <matheus1401@gmail.com>
2014-11-08 03:36:56 -0300

    Update and fixs some bugs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
   methodOverride = require('method-override'),
-  errorHandler = require('error-handler'),
+  errorHandler = require('errorhandler'),
   morgan = require('morgan'),
   routes = require('./routes'),
   api = require('./routes/api'),
@@ -25,15 +25,19 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
 
 var env = process.env.NODE_ENV || 'development';
 
 // development only
 if (env === 'development') {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 // production only
