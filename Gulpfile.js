8837ff3741ab4d85ed431904b6267a247c9586cb
Matheus Sampaio <matheus1401@gmail.com>
2015-03-10 18:40:54 -0300

    Adicionando Travis CI.
    - Commits no branch `master` vao iniciar um deploy para
    dev-wikifish.herokuapp.com
    - Commits no branch `production` vao iniciar um deploy para
    appwikifish.herokuapp.com

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,21 @@
 var path = require('path');
+var del = require('del');
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var clean = require('gulp-clean');
 var compass = require('gulp-compass');
-var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
+var minifyCSS= require('gulp-minify-css');
 
 var runSequence = require('run-sequence');
-
-var nodemon = require('gulp-nodemon');
 
 var dist = 'public';
 
 var src = ['src/**/*.js'];
 
-gulp.task('build-clean', function() {
-   return gulp.src(dist, {read: false})
-        .pipe(clean());
+gulp.task('build-clean', function(cb) {
+   del([
+       'public/**'
+   ], cb);
 });
 
 gulp.task('build-compass', function() {
@@ -38,23 +36,6 @@ gulp.task('build-convert-es6', function() {
         .pipe(gulp.dest(dist));
 });
 
-gulp.task('compress', function() {
-    return gulp.src(path.join(dist, '**/*.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(dist))
-});
-
 gulp.task('build', function() {
     runSequence('build-clean', ['build-convert-es6', 'build-compass']);
-});
-
-gulp.task('develop', function () {
-    nodemon({
-            script: 'app.js',
-            ext: 'scss js',
-            env: { 'NODE_ENV': 'development' },
-            ignore: ['public/**', '.sass-cache/**', 'node_modules/**']
-        })
-        .on('start', ['build'])
-        .on('change', ['build']);
 });
