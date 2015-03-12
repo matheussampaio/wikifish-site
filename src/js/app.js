6af7d027263f6c90917814113eac3032a6502333
Matheus Sampaio <matheus1401@gmail.com>
2015-03-11 21:30:48 -0300

    Estrutura de pastas refatorada

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,35 @@
-'use strict';
+(function () {
+    'use strict';
 
-// Declare app level module which depends on filters, and services
+    angular.module('wfApp', [
+        'ngRoute',
+        'ngResource',
+        'angular.filter',
 
-var wfApp = angular.module('wfApp', [
-    'ngRoute',
-    'ngResource',
-    'angular.filter',
+        'wfApp.controllers',
+        'wfApp.directives',
 
-    'wfApp.controllers',
-    'wfApp.directives',
+        'wfApp.new.filters',
+        'wfApp.new.directives',
+        'wfApp.new.services',
+        'wfApp.new.factories',
+        'wfApp.new.controllers',
+        'UserApp'
+    ])
+    .config(($routeProvider, $locationProvider) => {
+        $routeProvider.when('/', {templateUrl: 'partials/index', public: true});
 
-    'wfApp.new.filters',
-    'wfApp.new.directives',
-    'wfApp.new.services',
-    'wfApp.new.factories',
-    'wfApp.new.controllers',
-    'UserApp'
-]);
+        $routeProvider.when('/search', {templateUrl: 'partials/search', controller: 'wfSearchCtrl', public: true});
+        $routeProvider.when('/fish/:id', {templateUrl: 'partials/fish_detail', controller: 'wfFishDetailCtrl', public: true});
 
-wfApp.config(($routeProvider, $locationProvider) => {
-    $routeProvider.when('/', {templateUrl: 'partials/index', public: true});
+        $routeProvider.when('/login', {templateUrl: 'partials/login', login: true});
+        $routeProvider.when('/signup', { templateUrl: 'partials/signup', public: true});
 
-    $routeProvider.when('/search', {templateUrl: 'partials/search', controller: 'wfSearchCtrl', public: true});
-    $routeProvider.when('/fish/:id', {templateUrl: 'partials/fish_detail', controller: 'wfFishDetailCtrl', public: true});
+        $routeProvider.otherwise({ redirectTo: '/'});
 
-    $routeProvider.when('/login', {templateUrl: 'partials/login', login: true});
-    $routeProvider.when('/signup', { templateUrl: 'partials/signup', public: true});
-
-    $routeProvider.otherwise({ redirectTo: '/'});
-
-    $locationProvider.html5Mode(true);
-});
-
-wfApp.run((user) => {
-    user.init({ appId: '54fa33a3161df' });
-});
+        $locationProvider.html5Mode(true);
+    })
+    .run((user) => {
+        user.init({ appId: '54fa33a3161df' });
+    });
+})();
