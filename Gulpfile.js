5742470792731c806e39eb60f70136a22add2aa8
Matheus Sampaio <matheus1401@gmail.com>
2015-03-16 01:12:57 -0300

    Adicionado tratamento de error no gulpfile e index esta mais responsivo.

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,24 +7,26 @@ var browserSync = require('browser-sync');
 var runSequence = require('run-sequence');
 var stylish = require('jshint-stylish');
 
+var jade = require('gulp-jade');
 var babel = require('gulp-babel');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var compass = require('gulp-compass');
 var nodemon = require('gulp-nodemon');
+var plumber = require('gulp-plumber');
 var minifyCSS= require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
 var autoprefixer = require('gulp-autoprefixer');
+var templateCache = require('gulp-angular-templatecache');
 
-var distCSS = 'public/stylesheets';
-var distJS = 'public'
 
 /* ========= */
 /* VARIABLES */
 /* ========= */
 
+var distJS = 'public'
 var srcJS = [
     'src/**/app.module.js',
     'src/**/app.config.js',
@@ -32,13 +34,18 @@ var srcJS = [
     'src/**/*.js'
 ];
 
+var distCSS = 'public/stylesheets';
 var srcCSS = [
     'src/**/*.scss'
 ];
 
+var distJade = 'public/templates';
 var srcJade = [
     'views/**/*.jade'
 ];
+
+
+
 
 /* ================== */
 /* ONLY FOR DEVELOPER */
@@ -52,7 +59,8 @@ gulp.task('browser-sync', ['nodemon'], function() {
     browserSync.init(null, {
         proxy: 'http://localhost:3000',
         files: ['public/**/*.*'],
-        port: 7000
+        port: 7000,
+        open: false
     });
 
     gulp.watch(srcCSS, ['build:scss']);
@@ -85,6 +93,11 @@ gulp.task('build', function() {
 
 gulp.task('build:scss', function() {
     gulp.src(srcCSS)
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+        }}))
         .pipe(compass({
             project: __dirname,
             css: 'public/stylesheets',
@@ -100,7 +113,12 @@ gulp.task('build:scss', function() {
 
 
 gulp.task('build:js', function() {
-   return gulp.src(srcJS)
+    return gulp.src(srcJS)
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+        }}))
         .pipe(jshint())
         .pipe(jshint.reporter(stylish))
         .pipe(babel())
@@ -110,6 +128,22 @@ gulp.task('build:js', function() {
             .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(distJS));
+});
+
+
+
+gulp.task('build:templates', ['build:jade'], function () {
+    gulp.src(distJade + '/**/*.html')
+        .pipe(templateCache())
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('build:jade', function() {
+    gulp.src(srcJade)
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(distJade))
 });
 
 
